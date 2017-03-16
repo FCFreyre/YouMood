74a24f8af98f27767bb4744071677db3a082314e
F. Coulter Freyre <fcoulterfreyre@gmail.com>
2017-03-16 13:28:43 -0600

    added button paths

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 // youtube api key AIzaSyCzWYFUTwpc5KSRTzmsugcyvIxR2NuvB6w
 videoOptions = []
 
-$(document).ready(function(){          //once doc has loaded
-  $('#searchForm').submit(function(event) { // when submit is clicked
+$(document).ready(function(){               //once doc has loaded
+
+  $('.moodButton').click(function(event) {
     event.preventDefault()
+
+    $('#results').html('')           //resets result field on new search
     videoOptions = []
-    var input = $('#query').val()      //input = query value
-    youtube(input)                //call youtube with user input
+    var input = $(this).val()       //input = query value
+
+    switch (input) {
+      case "happy":
+        input = "the temptations"
+        break;
+      case "calm":
+        input = "ChillHop"
+        break;
+      case "sad":
+        window.confirm("Click OK if you'd like to wallow in your misery, and cancel if you want to turn that frown upside down!")
+        if(confirm) {
+          input = "depressing"
+        }else{
+          input = "cute+cats"
+        }
+        break;
+      case "angry":
+      window.confirm("Click OK if you'd like to feel the rage, and cancel if you need to cool it a bit")
+      if(confirm) {
+        input = "angry+death+metal"
+      }else{
+        input = "ChillHop"
+      }
+        break;
+      default:                   //goofy uses prompt of goofy, so it's default
+        break;
+    }
+    console.log(input)
+    youtube(input)                      //call youtube with user input
+  })
+
+  $('.searchForm').submit(function(event) { // when submit is clicked
+    event.preventDefault()
+    $('#results').html('')           //resets result field on new search
+    videoOptions = []
+    var input = $('#query').val()       //input = query value
+    youtube(input)                      //call youtube with user input
   })
 
 
@@ -14,7 +53,6 @@ $(document).ready(function(){          //once doc has loaded
 //get request to youtube for search results
 var youtube = function(input) {
   var url = "https://www.googleapis.com/youtube/v3/search?part=snippet,id&q="+input+"&type=video&key=AIzaSyCzWYFUTwpc5KSRTzmsugcyvIxR2NuvB6w"
-
 
   //lists first five videos of search, then appends random one of the five
   //to the results id
